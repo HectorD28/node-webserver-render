75c82c7234bdee41e6f26449c6f9ad1d1bcb61f0
hector <hectorda28yo@gmail.com>
2025-09-21 14:09:19 -0500

    Despliegue conectado con fly.io

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 
 app.set('view engine', 'hbs'); // para el uso de handlebars en express
